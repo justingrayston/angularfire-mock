1102a81113e3d6d7db5e3dba59767b6917d110ab
Justin Grayston <justingrayston@users.noreply.github.com>
2017-08-03 09:14:24 +0100

    Add initial stubs

--- src/main/database/mock-angularfire-database.ts
+++ src/main/database/mock-angularfire-database.ts
@@ -1,3 +1,31 @@
+import { FirebaseApp } from 'angularfire2/app';
+import * as firebase from 'firebase/app';
+import { Observable, BehaviorSubject } from 'rxjs/Rx';
+import { FirebaseListObservable } from 'angularfire2/database';
+
 export class MockAngularFireDatabase {
 
+  mockListResults: BehaviorSubject<any[]>;
+  mockObjectResult: BehaviorSubject<any>;
+
+  app: FirebaseApp;
+  database: firebase.database.Database;
+
+  constructor() {
+      this.resetMocks();
+  }
+
+  list(pathOrRef: string, opts?: Object): Observable<any[]> {
+      return this.mockListResults.asObservable();
+  }
+
+  object(pathOrRef: string, opts?: Object): Observable<any[]> {
+      return this.mockObjectResult.asObservable();
+  }
+
+  resetMocks() {
+      this.mockListResults = new BehaviorSubject(null);
+      this.mockObjectResult = new BehaviorSubject(null);
+  }
 }
+
